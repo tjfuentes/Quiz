09aab2e0836c9815a3faa597603d79c2e1680b4f
Tynneth Jhoel <tj.fuentes15@gmail.com>
2023-10-12 13:20:40 +0800

    initial commit

---
+++ src/components/Questions.jsx
@@ -0,0 +1,14 @@
+
+
+const Questions = () => {
+
+  
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Questions;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "../css/home.css"
+
+
+const Home = () => {
+
+  const [name, setName] = useState('Player name')
+
+  const player = (event) => {
+    setName(event.target.value)
+  }
+
+  return (
+    <>
+      <main className="home-container">
+
+        <div className="welcome">
+          <h1 className="back">Nulledge</h1>
+          <h1 className="front">Nulledge</h1>
+        </div>
+        
+        <div className="name">
+          <input type="text" value={name} onChange={player} maxLength={16} />
+        </div>
+
+        <div className="options">
+          <button className="play">Play</button>
+          <button>Options</button>
+          <button>High Scores</button>
+        </div>
+      </main>
+
+    </>
+  )
+}
+
+
+export default Home;
